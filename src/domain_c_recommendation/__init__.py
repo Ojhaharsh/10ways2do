c9416037da94f51437c6947ddc62cc348c030c08
ojha <harshvardhanojha900@gmail.com>
2026-03-29 01:20:12 +0530

    Add __init__.py for Domain C: Recommendation / Ranking module

---
+++ src/domain_c_recommendation/__init__.py
@@ -0,0 +1,8 @@
+"""
+Domain C: Recommendation / Ranking
+
+Approaches covering collaborative filtering to neural methods.
+"""
+
+from .data_generator import create_recommendation_dataset
+from .run_all import run_all_approaches
