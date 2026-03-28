153ab6b137df9d62a25fa99c77de654218d744dc
ojha <harshvardhanojha900@gmail.com>
2026-03-29 01:51:38 +0530

    Add __init__.py for Domain D: Time Series Forecasting module

---
+++ src/domain_d_time_series/__init__.py
@@ -0,0 +1,8 @@
+"""
+Domain D: Time Series Forecasting
+
+Approaches from statistical to deep learning methods.
+"""
+
+from .data_generator import create_timeseries_dataset
+from .run_all import run_all_approaches
